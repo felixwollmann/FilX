4ea4983ff8fa4b1a71779b93ab49734c6c683d93
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2025-02-18 08:25:22 +0100

    working proof of concept

---
+++ README.md
@@ -0,0 +1,11 @@
+# FilX
+A WebRTC file transfer proof of concept, created as part of a school project
+
+## Notes
+- https://github.com/priyangsubanerjee/webrtc-file-transfer/blob/master/public/index.html
+- https://webrtc-security.github.io/
+- https://webrtchacks.com/sdp-anatomy/
+- https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Connectivity
+- https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation
+- Beispiel von WebRTC: https://github.com/webrtc/samples/blob/gh-pages/src/content/datachannel/filetransfer/js/main.js
+- 
--- filx-frontend/src/views/HomeView.vue
+++ filx-frontend/src/views/HomeView.vue
@@ -1,9 +1,111 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, useTemplateRef } from "vue";
+
+const configuration = {
+  // iceServers: [{ urls: "stun:stun.l.google.com:5349" }],
+  iceServers: [{ urls: "stun:freestun.net:3478" }],
+};
+
+const peerConnection = new RTCPeerConnection(configuration);
+let dataChannel = peerConnection.createDataChannel("data");
+dataChannel.addEventListener("open", (event) => {
+  console.log("YAAAAAY", event);
+});
+
+dataChannel.addEventListener("message", handleMessage);
+
+console.log("peerConnection:", peerConnection);
+const offer = await peerConnection.createOffer();
+await peerConnection.setLocalDescription(offer);
+
+// peerConnection.setRemoteDescription
+
+peerConnection.addEventListener("icecandidate", (event) => {
+  console.log("got ice candidate", event);
+});
+
+const offerIsFinished = ref(false);
+
+peerConnection.addEventListener("icegatheringstatechange", (event) => {
+  if (peerConnection.iceGatheringState === "complete") {
+    console.log(
+      "All ICE candidates gathered:",
+      event,
+      peerConnection.localDescription
+    );
+    // Now send the offer to the other party
+    // console.log(peerConnection.localDescription);
+    offerIsFinished.value = true;
+  }
+});
+
+console.log("offer:", offer);
+
+function copyOffer() {
+  console.log("copying offer");
+  window.navigator.clipboard.writeText(
+    JSON.stringify(peerConnection.localDescription?.toJSON())
+  );
+}
+
+const remoteOfferInput = ref("");
+const remoteAnswerInput = ref("");
+
+async function respondToRemoteOffer() {
+  await peerConnection.setRemoteDescription(JSON.parse(remoteOfferInput.value));
+  await peerConnection.setLocalDescription();
+  await new Promise<void>((resolve, reject) => {
+    peerConnection.addEventListener("icegatheringstatechange", (event) => {
+      if (peerConnection.iceGatheringState === "complete") {
+        resolve();
+      }
+    });
+  });
+  window.navigator.clipboard.writeText(
+    JSON.stringify(peerConnection.localDescription?.toJSON())
+  );
+  peerConnection.addEventListener("datachannel", (event) => {
+    console.log('YAY 2');
+    dataChannel = event.channel;
+    dataChannel.addEventListener("message", handleMessage);
+  });
+}
+
+async function connectWithRemoteAnswer() {
+  await peerConnection.setRemoteDescription(
+    JSON.parse(remoteAnswerInput.value)
+  );
+}
+
+async function handleMessage(event: MessageEvent) {
+  console.log('Recieved message:', event.data);
+}
+
+async function sendMessage() {
+  const payload = `${Math.random()}`;
+  console.log('Sending Message:', payload);
+  dataChannel.send(payload);
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>Trying out WebRTC.</p>
+    <button @click="copyOffer" class="bg-amber-200 p-1 rounded">
+      Copy offer: {{ offerIsFinished ? "Offer ready" : "Making offer..." }}
+    </button>
+
+    <br>
+    <textarea v-model="remoteOfferInput"></textarea>
+    <button @click="respondToRemoteOffer">Copy answer</button>
+    <br>
+    <textarea v-model="remoteAnswerInput"></textarea>
+    <button @click="connectWithRemoteAnswer">Connect to answer</button>
+    <br>
+    <button @click="sendMessage">Send hello message</button>
+    
+    
+
+    <!-- <code>{{ peerConnection.localDescription?.sdp }}</code> -->
   </main>
 </template>
